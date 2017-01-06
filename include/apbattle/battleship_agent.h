05f3a76f9625710ddcbc2ac5256ed35ded096a35
abrinkmacmu <abrinkma@andrew.cmu.edu>
2017-01-06 09:42:01 -0500

    added outline for agent api

--- include/apbattle/battleship_agent.h
+++ include/apbattle/battleship_agent.h
@@ -1,4 +1,38 @@
 #ifndef __battleship__agent__
 #define __battleship__agent__
 
+namespace bship {
+
+class BattleshipAgent{
+public:
+
+	/**
+	 * @brief spins up reading thread and allocates fields
+	 */
+	BattleshipAgent(
+		std::string host, std::string host_port
+		std::string client, std::string client_port);
+
+	virtual ~BattleshipAgent();
+
+	void playGame();
+
+private:
+
+	void connectToSocket();
+
+	virtual void guessLocation();
+
+	void respondToRequest();
+
+	void logMoves();
+
+	void sendMessage();
+
+	void receiveMessage();
+
+}; // class BattleshipAgent
+
+} // ns bship
+
 #endif
